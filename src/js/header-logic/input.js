5e8b8f2130a1f10a7f92dfa54e5c85c5c0b8fb11
Viktoriia212022 <3derudit@gmail.com>
2023-01-15 15:25:35 +0200

    add header new and js

--- src/js/header-logic/input.js
+++ src/js/header-logic/input.js
@@ -1,28 +1,23 @@
-const API_KEY = 'fab59cb7fc5906252e3bc057965c8a74';
-const URL = 'https://api.themoviedb.org/3/search/movie?';
+import PopularMovieFromServer from './feach-api';
+const PopularMovieFromServer = new PopularMovieFromServer();
 
+const formEl = document.querySelector('.home_header-form');
+const searchBtn = document.querySelector('.home_header-btn');
+const sectionEl = document.querySelector('.film-card-section');
 
-
-function fetch() {
-   fetch(`${URL}api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=soul`)
-    .then(response => {
-        return response.json()
-    });
-}
-
-
-const inputEl = document.querySelector('.home_header-input');
-inputEl.addEventListener('submit', onSearchForm);
+formEl.addEventListener('submit', onSearchForm);
 
 function onSearchForm(e) {
   e.preventDefault();
 
-const inputValue = e.currentTarget.elements.search.value;
-  fetch()   
+  const inputValue = e.currentTarget.elements.query.value;
+  
+   PopularMovieFromServer.getPopularMovieFromServer(inputValue);
+  
+  if (inputValue === '') {
+    sectionEl.innerHTML = '';
+    searchBtn.style.visibility = 'visible';
+  };
+}
 
-};
- 
-// function clearContent() {
-//   countryListEll.innerHTML = '';
-//   countryInfoEll.innerHTML = '';
-// }
+
