cceb53c0a8f37bea37784b4dafb33f1c838973eb
Viktoriia212022 <3derudit@gmail.com>
2023-01-15 19:22:14 +0200

    add js2

--- src/js/header-logic/input.js
+++ src/js/header-logic/input.js
@@ -1,23 +1,23 @@
-import PopularMovieFromServer from './feach-api';
-const PopularMovieFromServer = new PopularMovieFromServer();
+// import PopularMovieFromServer from './feach-api';
+// const PopularMovieFromServer = new PopularMovieFromServer();
 
 const formEl = document.querySelector('.home_header-form');
 const searchBtn = document.querySelector('.home_header-btn');
-const sectionEl = document.querySelector('.film-card-section');
+const sectionEl = document.querySelector('.film-list');
 
 formEl.addEventListener('submit', onSearchForm);
 
 function onSearchForm(e) {
   e.preventDefault();
 
-  const inputValue = e.currentTarget.elements.query.value;
+  const inputValue = e.currentTarget.elements.query.value.trim();
   
-   PopularMovieFromServer.getPopularMovieFromServer(inputValue);
+  //  PopularMovieFromServer.getPopularMovieFromServer(inputValue);
   
   if (inputValue === '') {
     sectionEl.innerHTML = '';
     searchBtn.style.visibility = 'visible';
-  };
+      };
 }
 
 
