ddbd58ce1202d39dba3f8b0d0e3ebc462172e72c
Vitalii Melnyk <melnikvitaliy30@gmail.com>
2023-01-16 19:01:16 +0200

    Виправив кількість жанрів та додав scale на картки фільмів

--- src/fetch_api.js
+++ src/fetch_api.js
@@ -1,5 +1,5 @@
 
-export default class PopularMovieFromServer {
+export default class MovieFromServer {
   constructor () {
     this.currentPage  = 1;
   }
@@ -7,7 +7,7 @@ export default class PopularMovieFromServer {
   getPopularMovieFromServer() {
     const API_KEY = '197007a676a4d3e84c6bdb6f21b18a1a';
     return fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}&page=${this.page}&language=uk`)
-    .then(response => response.json())
+    .then(response => response.json()).catch(console.log)
   }
 
   get page() {
--- src/popularMovies.js
+++ src/popularMovies.js
@@ -1,6 +1,4 @@
-import PopularMovieFromServer from './fetch_api';
-import Pagination from 'tui-pagination';
-// import 'tui-pagination/dist/tui-pagination.css';
+import MovieFromServer from './fetch_api';
 
 
 const genresInfo = [
@@ -47,64 +45,55 @@ const genresInfoUk = [
   { id: 37, name: 'Вестерн' },
 ];
 
-const mainListRef = document.querySelector('.film-list')
+const mainListRef = document.querySelector('.film-list');
+const formRef = document.querySelector('#search-form');
 
 
-const popularMovieFromServer = new PopularMovieFromServer;
+const movieFromServer = new MovieFromServer;
 
 addPopularMovieToPage();
-paginationOnPage();
+
 
 // Функция ожидает номер страницы, делает запрос на сервер и рендерит разметку
 async function addPopularMovieToPage(newPage = 1) {
-  popularMovieFromServer.page = newPage;
-  const popularMovie = await popularMovieFromServer.getPopularMovieFromServer().then(data => {
+  movieFromServer.page = newPage;
+  const popularMovie = await movieFromServer.getPopularMovieFromServer().then(data => {
     return data;
   });
   mainListRef.innerHTML = '';
 
   addMurkupOnPage(popularMovie.results)
 }
+
 // Функция ожидает массив объектов и рендерит разметку карточек фильмов на страницу
 function addMurkupOnPage(array) {
+  const url = 'src="https://media.istockphoto.com/id/984996502/uk/%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80%D0%BD%D1%96-%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%BD%D1%8F/%D0%BD%D0%B5%D0%B7%D0%B0%D0%B1%D0%B0%D1%80%D0%BE%D0%BC-%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80%D0%BD%D0%B8%D0%B9-%D0%B4%D0%B8%D0%B7%D0%B0%D0%B9%D0%BD-%D1%88%D0%B0%D0%B1%D0%BB%D0%BE%D0%BD%D1%83.jpg?s=612x612&w=0&k=20&c=fWxMbUFQMxaL5_wB6SjZ9LLGnvpkCYAIdpqgde_ieR4="';
   const murkupFromArray = array.map(({id, poster_path, title, genre_ids, release_date}) => {
     const imageUrl = `src="https://www.themoviedb.org/t/p/w500/${poster_path}"`;
-    const genre = getGenreArrayForOnCard(genre_ids);
+    const genre = getGenreArrayForOneCard(genre_ids);
+    let nameOfGenre = '';
     if (genre.length === 1) {
+      nameOfGenre = genre[0];
+    } else if (genre.length === 2) {
+      nameOfGenre = genre[0] + ', ' + genre[1];
+    } else if (genre.length === 3) {
+      nameOfGenre = genre[0] + ', ' + genre[1] + ', ' + genre[2];
+    } else {
+      nameOfGenre = genre[0] + ', ' + genre[1] + ', ' + 'Інше';
+    }
       return `<li class="film-item" id="${id}">
-    <img width="280" class="film-img" ${imageUrl}" alt="${title}" />
+    <img width="280" class="film-img" ${poster_path ? imageUrl: url}" alt="${title}" />
     <p class="film-name">
       ${title} <br />
-      <span class="film-tag">${genre[0]} | ${release_date.slice(0, 4)}</span>
+      <span class="film-tag">${nameOfGenre} | ${release_date ? release_date.slice(0, 4) : 'Немає дати випуску'}</span>
     </p>
   </li>`
-    }
-    if (genre.length > 1 && genre.length < 3) {
-      return `<li class="film-item" id="${id}">
-    <img width="280" class="film-img" ${imageUrl}" alt="${title}" />
-    <p class="film-name">
-      ${title} <br />
-      <span class="film-tag">${genre[0]}, ${genre[1]} | ${release_date.slice(0, 4)}</span>
-    </p>
-  </li>`
-    }
-    if (genre.length > 2) {
-      return `<li class="film-item" id="${id}">
-    <img width="280" class="film-img" ${imageUrl}" alt="${title}" />
-    <p class="film-name">
-      ${title} <br />
-      <span class="film-tag">${genre[0]}, ${genre[1]}, Інше | ${release_date.slice(0, 4)}</span>
-    </p>
-  </li>`
-    }
-    // console.log(poster_path)
-    
   }).join('');
   mainListRef.insertAdjacentHTML('beforeend', murkupFromArray)
 }
 
 // Функция ожидает массив id в виде чисел и возвращает массив жанров для конкретного фильма
-function getGenreArrayForOnCard(genresIds) {
+function getGenreArrayForOneCard(genresIds) {
   const genresArrayForOnCard = [];
   
   for (const genresId of genresIds) {
@@ -116,47 +105,4 @@ function getGenreArrayForOnCard(genresIds) {
   }
   
   return genresArrayForOnCard;
-}
-
-
-async function paginationOnPage() {
-  const totalPages = await popularMovieFromServer.getPopularMovieFromServer().then(data => {
-    // console.log(data.total_pages);
-    return data.total_pages;
-  }); 
-  const opt = paginationOptions(totalPages)
-  const pagination = new Pagination('pagination', opt);
-  pagination.on('beforeMove', (evt) => {
-    // console.log(evt.page)
-    const currentPage = evt.page;
-    addPopularMovieToPage(currentPage)
-  })
-}
-
-function paginationOptions(amount) {
-  const options = {
-    totalItems: amount,
-    itemsPerPage: 20,
-    visiblePages: 5,
-    page: 1,
-    centerAlign: true,
-
-    template: {
-      page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-          currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-              moveButton:
-      '<a href="#" class="tui-page-btn tui-{{type}} custom-class-{{type}}">' +
-          '<span class="tui-ico-{{type}}">:::</span>' +
-          '</a>',
-          disabledMoveButton:
-      '<span class="tui-page-btn tui-is-disabled tui-{{type}} custom-class-{{type}}">' +
-          '<span class="tui-ico-{{type}}">:::</span>' +
-          '</span>',
-          moreButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip custom-class-{{type}}">...' +
-          '<span class="tui-ico-ellip"></span>' +
-          '</a>',
-    },
-  };
-  return options;
 }
