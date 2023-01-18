57184346762568302c7e3519407c1275969c368e
Vol4enak <52600927+Vol4enak@users.noreply.github.com>
2023-01-18 22:01:39 +0200

    Revert "add pagination"

--- src/fetch_api.js
+++ src/fetch_api.js
@@ -1,20 +1,19 @@
-export class PopularMovieFromServer {
+export default class PopularMovieFromServer {
   constructor() {
     this.currentPage = 1;
-    this.searchQuery = '';
   }
 
   getPopularMovieFromServer() {
     const API_KEY = '197007a676a4d3e84c6bdb6f21b18a1a';
     return fetch(
-      `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}&page=${this.page}`
+      `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}&page=${this.page}&language=uk`
     ).then(response => response.json());
   }
 
-  getSubmitMovieFromServer() {
+  getSubmitMovieFromServer(filmName) {
     const API_KEY = '197007a676a4d3e84c6bdb6f21b18a1a';
     return fetch(
-      `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&page=${this.page}&query=${this.searchQuery}&include_adult=false`
+      `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&page=${this.page}&query=${filmName}`
     ).then(response => response.json());
   }
 
@@ -24,13 +23,5 @@ export class PopularMovieFromServer {
 
   set page(pageNumber) {
     this.currentPage = pageNumber;
-  }
-
-  get query() {
-    return this.searchQuery;
-  }
-
-  set query(newQuery) {
-    this.searchQuery = newQuery;
   }
 }
--- src/js/header-logic/input.js
+++ src/js/header-logic/input.js
@@ -1,62 +1,116 @@
-import {PopularMovieFromServer} from '../../fetch_api';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import {addMurkupOnPage, paginationOptions} from '../../popularMovies';
-import Pagination from 'tui-pagination';
-
+import PopularMovieFromServer from '../../fetch_api';
 const popularMovieFromServer = new PopularMovieFromServer();
-
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const formEl = document.querySelector('.home_header-form');
 const searchBtn = document.querySelector('.home_header-btn');
 const mainListRef = document.querySelector('.film-list');
-const paginationBox = document.querySelector('#pagination');
 
 formEl.addEventListener('submit', searchFilm);
 
 async function searchFilm(e) {
   e.preventDefault();
-  paginationBox.innerHTML = '';
-  let searchMovieValue = e.target.elements.query.value;
 
-  if (!Boolean(searchMovieValue.trim())) {
+  if (!Boolean(formEl[0].value.trim())) {
     searchBtn.style.visibility = 'visible';
-    Notify.warning('Enter the name of movie.');
+    Notify.warning('Введіть назву фільму.');
     return;
   }
-  
-  addSearchingMovieToPage(searchMovieValue);
-  paginationForSearchFetch();
-}
+  const searchMovie = await popularMovieFromServer
+    .getSubmitMovieFromServer(formEl[0].value.trim())
+    .then(data => {
+      return data;
+    });
 
-// Пагинация для поиска
-async function paginationForSearchFetch() {
-  const totalPages = await popularMovieFromServer.getSubmitMovieFromServer().then(data => {
-    // console.log(data);
-    return data.total_pages;
-  });
-  const options = paginationOptions(totalPages);
-  const searchPagination = new Pagination('pagination', options);
-  
-  searchPagination.on('beforeMove', (evt) => {
-    const currentPage = evt.page;
-    addSearchingMovieToPage(popularMovieFromServer.query, currentPage)
-  })
-
-
-// Функция ожидает значение из строки поиска и номер страницы и рендерит разметку
-async function addSearchingMovieToPage(value, searchPage) {
-  popularMovieFromServer.page = searchPage;
-  popularMovieFromServer.query = value;
-  const searchingMovieData = await popularMovieFromServer.getSubmitMovieFromServer().then(data => {
-    return data;
-  });
-
-  if (searchingMovieData.total_results == 0) {
-    Notify.warning('Sorry! We did not find this movie.');
+  if (searchMovie.total_results == 0) {
+    Notify.warning('Такого фільму немає.');
     return;
   }
+
   mainListRef.innerHTML = '';
 
-  addMurkupOnPage(searchingMovieData.results)
+  addMurkupOnPage(searchMovie.results);
 }
 
+//нижче функції по рендерингу карток
+
+// Функция ожидает массив объектов и рендерит разметку карточек фильмов на страницу
+function addMurkupOnPage(array) {
+  const murkupFromArray = array
+    .map(({ id, poster_path, title, genre_ids, release_date }) => {
+      const imageUrl = `src="https://www.themoviedb.org/t/p/w500/${poster_path}"`;
+      const genre = getGenreArrayForOnCard(genre_ids);
+      if (genre.length === 1) {
+        return `<li class="film-item" id="${id}">
+    <img width="280" class="film-img" ${imageUrl}" alt="${title}" />
+    <p class="film-name">
+      ${title} <br />
+      <span class="film-tag">${genre[0]} | ${release_date.slice(0, 4)}</span>
+    </p>
+  </li>`;
+      }
+      if (genre.length > 1 && genre.length < 3) {
+        return `<li class="film-item" id="${id}">
+		  <img width="280" class="film-img" ${imageUrl}" alt="${title}" />
+    <p class="film-name">
+      ${title} <br />
+      <span class="film-tag">${genre[0]}, ${genre[1]} | ${release_date.slice(
+          0,
+          4
+        )}</span>
+    </p>
+  </li>`;
+      }
+      if (genre.length > 2) {
+        return `<li class="film-item" id="${id}">
+    <img width="280" class="film-img" ${imageUrl}" alt="${title}" />
+    <p class="film-name">
+      ${title} <br />
+      <span class="film-tag">${genre[0]}, ${
+          genre[1]
+        }, Other | ${release_date.slice(0, 4)}</span>
+    </p>
+  </li>`;
+      }
+    
+    })
+    .join('');
+  mainListRef.insertAdjacentHTML('beforeend', murkupFromArray);
+}
+
+// Функция ожидает массив id в виде чисел и возвращает массив жанров для конкретного фильма
+function getGenreArrayForOnCard(genresIds) {
+  const genresArrayForOnCard = [];
+
+  for (const genresId of genresIds) {
+    genresInfo.map(genre => {
+      if (genresId === genre.id) {
+        genresArrayForOnCard.push(genre.name);
+      }
+    });
+  }
+
+  return genresArrayForOnCard;
+}
+
+const genresInfo = [
+  { id: 28, name: 'Action' },
+  { id: 12, name: 'Adventure' },
+  { id: 16, name: 'Animation' },
+  { id: 35, name: 'Comedy' },
+  { id: 80, name: 'Crime' },
+  { id: 99, name: 'Documentary' },
+  { id: 18, name: 'Drama' },
+  { id: 10751, name: 'Family' },
+  { id: 14, name: 'Fantasy' },
+  { id: 36, name: 'History' },
+  { id: 27, name: 'Horror' },
+  { id: 10402, name: 'Music' },
+  { id: 9648, name: 'Mystery' },
+  { id: 10749, name: 'Romance' },
+  { id: 878, name: 'Science Fiction' },
+  { id: 10770, name: 'TV Movie' },
+  { id: 53, name: 'Thriller' },
+  { id: 10752, name: 'War' },
+  { id: 37, name: 'Western' },
+];
