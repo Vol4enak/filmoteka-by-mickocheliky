474ba7a4348c9a00c214938aa4143c808911e28e
Volodymyr Butyter <butytervolodia@gmail.com>
2023-01-18 22:59:02 +0200

    modal auth added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import './popularMovies';
 import './js/body-logic/pagination';
 import './js/header-logic/input';
+import './js/body-logic/auth-site-change';
+import './js/body-logic/sign-modal'
+import './js/body-logic/authentification'
