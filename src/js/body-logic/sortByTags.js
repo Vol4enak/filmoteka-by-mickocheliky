594f0b648ab28f988606866937373aae936b6f3e
Vol4enak <52600927+Vol4enak@users.noreply.github.com>
2023-01-20 07:10:33 +0200

    Update sortByTags.js

--- src/js/body-logic/sortByTags.js
+++ src/js/body-logic/sortByTags.js
@@ -117,12 +117,6 @@ function renderTagsList(tags) {
   tagsList.innerHTML = markup;
 }
 
-function createTag({ id, name }) {
-  return `
-      <li  class="menu-tags__item">
-        <a data-id="${id}" class="menu-tags__link" href="./">${name}</a></li>
-`;
-}
 
 function renderTagsList(tags) {
   const markup = tags.map(createTag).join('');
